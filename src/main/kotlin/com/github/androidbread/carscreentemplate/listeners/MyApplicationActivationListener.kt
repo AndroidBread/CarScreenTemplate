94c6c09761972943cfde85afb25cd3f903d1647f
GitHub Action <action@github.com>
2024-02-01 06:06:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/androidbread/carscreentemplate/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/androidbread/carscreentemplate/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.androidbread.carscreentemplate.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
